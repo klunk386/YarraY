d750a9aaf748b0d911c85b327f00e197b2cf0ec5
grendel <poggi.valerio@gmail.com>
2017-10-28 08:37:48 +0200

    Init Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='YarraY',
+      version='0.0.1',
+      description='A visual analyser of seismic recordings',
+      url='https://github.com/klunk386/YarraY',
+      author='Valerio Poggi',
+      author_email='poggi.valerio@gmail.com',
+      license='GPL3'
+      packages=['yarray'],
+      zip_safe=False)
